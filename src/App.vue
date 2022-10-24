0b38f1e6d69feb49c4fccf0851b515ab12c021ca
Sarah Moe <79814626+s-moe@users.noreply.github.com>
2022-10-24 13:58:59 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <PxErrorPage message="This is an error message" icon=""></PxErrorPage>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 in CodeSandbox!" />
+</template>
+
+<script>
+import HelloWorldVue from "./components/HelloWorld.vue";
+import PxErrorPage from "./components/PxErrorPage.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld: HelloWorldVue,
+    PxErrorPage,
+  },
+  // errorCaptured(err, comp, info) {
+  //   this.$refs.err.innerHTML = "we have an error";
+  //   console.log(err);
+  // },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+/* .err {
+  position: fixed;
+  z-index: 9999;
+  padding: 28px;
+  background-color: red;
+} */
+</style>
---
+++ src/components/PxErrorPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div></div>
+    <h1>{{ status - code }}</h1>
+    <h3>{{ message }}</h3>
+  </div>
+</template>
+<script>
+export default {
+  name: "px-error-page",
+  props: {
+    statusCode: { type: Number },
+    message: { type: String, default: "Uh-oh! There is an error" },
+    detail: { type: String, default: "Default message for detail string" },
+    icon: { type: String },
+    actions: { type: Array },
+  },
+};
+</script>
+<style>
+h1 {
+  color: blue;
+}
+</style>
